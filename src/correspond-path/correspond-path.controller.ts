c16da336448c1f0cbc0ca82879630e7d2cee43dd
HMXHKAKA <97349507+Dreamkaka@users.noreply.github.com>
2024-02-10 22:22:06 +0800

    Update correspond-path.controller.ts
    忘记写注释了

--- src/correspond-path/correspond-path.controller.ts
+++ src/correspond-path/correspond-path.controller.ts
@@ -2,11 +2,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { CorrespondPathService } from './correspond-path.service';
 import * as moment from 'moment-timezone';
+import { ApiTags,ApiOperation } from '@nestjs/swagger';
 
 @Controller('crypto')
 export class CorrespondPathController {
   constructor(private readonly CorrespondPathService : CorrespondPathService ) {}
-
+  
+  @ApiTags('有用的工具')
+  @ApiOperation({ summary: '生成CorrespondPath' })
   @Get('')
   getHash(): { timestamp: string, hash: string } {
     const timestamp = moment().tz('Asia/Shanghai').format('x');
