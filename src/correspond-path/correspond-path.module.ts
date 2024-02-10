88b0d84946d0854431495da5ba3730e06ac73d04
HMXHKAKA <97349507+Dreamkaka@users.noreply.github.com>
2024-02-10 22:54:23 +0800

    remove
    crash

--- src/correspond-path/correspond-path.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CorrespondPathService } from './correspond-path.service';
-import { CorrespondPathController } from './correspond-path.controller';
-
-@Module({
-  providers: [CorrespondPathService],
-  controllers: [CorrespondPathController]
-})
-export class CorrespondPathModule {}
